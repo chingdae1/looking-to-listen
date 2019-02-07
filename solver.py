eb2ba37e63b8b010364312eebe0f8c3a7bae4b1d
chingdae1 <chingdae0820@hanmail.net>
2019-02-07 14:00:10 +0900

    debug

--- solver.py
+++ solver.py
@@ -160,6 +160,8 @@ class Solver():
             mix_path = os.path.join(batch_dir, 'mix.wav')
             Solver.spect_to_wav(mix, mix_path)
             idx_batch = idx_tensor[i]
+            print(idx_tensor)
+            print(idx_batch)
 
             for k, s in enumerate(separated):
                 gt_dir = os.path.join(batch_dir, 'groud_truth')
