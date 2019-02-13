a27c882d59c98e633227842dcdd9969a051ad3f9
chingdae1 <chingdae0820@hanmail.net>
2019-02-13 12:18:54 +0900

    train

--- solver.py
+++ solver.py
@@ -12,7 +12,7 @@ class Solver():
         self.config = config
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.train_data = Dataset(data_dir=config['data_dir'],
-                                  mode='toy')
+                                  mode='train')
         self.train_loader = DataLoader(self.train_data,
                                        batch_size=config['batch_size'],
                                        num_workers=config['num_workers'],
